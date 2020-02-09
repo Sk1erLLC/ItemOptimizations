25c4e986ad9f8db6f2ae641b662b7ebfe83ac63e
asbyth <cyronize@gmail.com>
2020-02-09 14:47:12 -0500

    increase version bc cache yay

--- src/main/java/club/sk1er/items/ItemOptimization.java
+++ src/main/java/club/sk1er/items/ItemOptimization.java
@@ -2,6 +2,6 @@ package club.sk1er.items;
 
 import net.minecraftforge.fml.common.Mod;
 
-@Mod(modid = "item_optimization", name = "Item Optimization", version = "1.1")
+@Mod(modid = "item_optimization", name = "Item Optimization", version = "1.2")
 public class ItemOptimization {
 }
