52a5d30067ec606c5bf7a0fea08a0cac2421f45b
asbyth <cyronize@gmail.com>
2020-02-09 14:41:40 -0500

    fix version

--- src/main/java/club/sk1er/items/ItemOptimization.java
+++ src/main/java/club/sk1er/items/ItemOptimization.java
@@ -2,6 +2,6 @@ package club.sk1er.items;
 
 import net.minecraftforge.fml.common.Mod;
 
-@Mod(modid = "item_optimization", name = "Item Optimization", version = "1.0")
+@Mod(modid = "item_optimization", name = "Item Optimization", version = "1.1")
 public class ItemOptimization {
 }
